e1da34028c460ef6de44a3bbc46a8c288cd5a83b
Kcee Landon <landonkcee@gmail.com>
2024-04-26 16:03:16 -0700

    finish etch a sketch

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.style.border = "1px solid black";
-    newDiv.style.height = "200px";
-    newDiv.style.width = "200px";
-    newDiv.style.flex = "1 0 20%";
-    newDiv.classList.add("" + i);
-    container.appendChild(newDiv);
+let button = document.createElement("button");
+button.textContent = "Reset";
+button.classList.add("prompt");
+document.body.appendChild(button);
+// let numTilesOnEdge = 4; // should be replaced later with the value from the prompt
+function initializeApp() {
+    createGrid(4);
 }
+function createGrid(numCols) {
+    for (let i = 0; i < numCols; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.style.display = "flex";
+        newDiv.style.flexDirection = "column";
+        for (let i = 0; i < numCols; i++) {
+            let cell = document.createElement("div");
+            let cellSize = container.offsetHeight / numCols;
+            cell.style.border = "1px solid black";
+            cell.style.height = cellSize + "px";
+            cell.style.width = cellSize + "px";
+            cell.classList.add("cell");
+            cell.addEventListener("mouseover", () => {
+                cell.style.backgroundColor = "black";
+            })
+            newDiv.appendChild(cell);
+        }
+        newDiv.classList.add("col");
+        container.appendChild(newDiv);
+    }
+}
+
+function clearGrid() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+initializeApp();
+button.addEventListener("click", () => {
+    let userInput = prompt("Please enter a value for the grid.");
+    let numTilesOnEdge = parseInt(userInput);
+    while (numTilesOnEdge <= 0 || numTilesOnEdge > 100) {
+        userInput = prompt("Please enter a value between 1 and 100.");
+        numTilesOnEdge = parseInt(userInput);
+    }
+    clearGrid();
+    createGrid(numTilesOnEdge);
+})
